f21fedc702d56c3de80f4004789aae5c33735ce1
Eric Hertwig <eric@ehdev.de>
2019-12-25 15:12:50 +0100

    Add some wiring

--- src/DependencyInjection/AnonymizerExtension.php
+++ src/DependencyInjection/AnonymizerExtension.php
@@ -2,19 +2,20 @@
 
 declare(strict_types=1);
 
-namespace PHPSharkTank\Anonymizer\DependencyInjection;
+namespace PHPSharkTank\AnonymizerBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
 
-class PHPSharkTankAnonymizeExtension extends Extension
+class AnonymizerExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('doctrine.yaml');
-        $loader->load('faker.yaml');
+        foreach (['faker', 'services'] as $value) {
+            $loader->load(sprintf('%s.yaml', $value));
+        }
     }
 }
